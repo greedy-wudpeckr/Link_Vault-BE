6720ff90cae8bd789438db128e9f6ec83d99223c
greedy-wudpeckr <mudituiet@gmail.com>
2025-01-21 00:37:03 +0530

    origin

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: "*", // Replace with your frontend URL
     credentials: true, // Allow cookies to be sent
   })
 );
